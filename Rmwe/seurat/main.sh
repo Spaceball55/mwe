e59a116af97cd3ad9f78887046dc5dbc571ba674
Spaceball55 <davespeed50@gmail.com>
2023-06-07 15:54:01 -0500

    seurat tutorial

--- Rmwe/seurat/main.sh
+++ Rmwe/seurat/main.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-# R -e "source('main.R', echo=T)"
-Rscript main.R
+# Rscript main.R
+outdir=$(mrrdir.sh)
+mkdir -p "$outdir"
+
+slurmtaco.sh -n g00 -t 1 -m 10G -- "$(cat <<EOF
+R --vanilla \
+	-e "outdir='$outdir'" \
+	-e "source('tutorial.R', echo=T)"
+EOF
+)"
